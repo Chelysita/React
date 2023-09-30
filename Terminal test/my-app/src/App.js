fb5aa470eb9b5c8266138559e7a07589e6efbb20
Chelysita <ara.medinago@gmail.com>
2023-09-30 10:36:32 -0400

    Added files to new repository

---
+++ Terminal test/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hola</p>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
