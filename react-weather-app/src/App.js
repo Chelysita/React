1c04755ee4224827e646f988ccb407804b2d167c
Chelysita <ara.medinago@gmail.com>
2023-10-13 14:37:31 -0400

    Adding files

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>If you dream it, you are halfway there</p>
+      </header>
+    </div>
+  );
+}
